814a606aea19e31c93fe8d161d69667aa36673d1
no2drm <148765991+no2drm@users.noreply.github.com>
2024-04-16 17:34:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 
 def setup_solver(): #please body dont sue me
     if not os.path.exists("utils"): os.mkdir("utils")
-    files = ["https://raw.githubusercontent.com/Body-Alhoha/turnaround/main/utils/solver.py", "https://raw.githubusercontent.com/Body-Alhoha/turnaround/main/utils/page.html"]
+    files = ["https://raw.githubusercontent.com/no2drm/turnaround/main/utils/solver.py", "https://raw.githubusercontent.com/no2drm/turnaround/main/utils/page.html"]
     for file in files:
         r = requests.get(file).text
         with open("utils/" + file.split("/")[-1], "w") as f:
